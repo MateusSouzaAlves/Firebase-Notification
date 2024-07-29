1f0a3258c31633c4048d133ffdfa8a793321305f
MateusSouzaAlves <solution.mateus@gmail.com>
2024-07-29 12:09:40 -0300

    REcebendo notificações simples em primeiro plano

--- app/src/main/java/com/example/firebase_eva/firebase/FirebaseService.java
+++ app/src/main/java/com/example/firebase_eva/firebase/FirebaseService.java
@@ -1,7 +1,18 @@
 package com.example.firebase_eva.firebase;
 
-import androidx.annotation.NonNull;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.example.firebase_eva.MainActivity;
+import com.example.firebase_eva.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -9,7 +20,36 @@ public class FirebaseService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage message) {
-        super.onMessageReceived(message);
+        if (message.getNotification() != null){
+            String title = message.getNotification().getTitle();
+            String messageBody = message.getNotification().getBody();
+
+            sendNotification(title, messageBody);
+
+        }
+    }
+
+    private void sendNotification(String title,String message){
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
+
+        String channel = getString(R.string.default_notification_channel_id);
+
+        Uri sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this, channel)
+                .setSmallIcon(R.drawable.ic_laucher_eva)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setSound(sound)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification.build());
     }
 
     @Override
