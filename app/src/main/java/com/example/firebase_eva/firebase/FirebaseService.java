464ac199b09c5056829780823d44fe6a845dec68
MateusSouzaAlves <solution.mateus@gmail.com>
2024-07-29 11:13:24 -0300

    REcebendo notificações simples

---
+++ app/src/main/java/com/example/firebase_eva/firebase/FirebaseService.java
@@ -0,0 +1,19 @@
+package com.example.firebase_eva.firebase;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FirebaseService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage message) {
+        super.onMessageReceived(message);
+    }
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+    }
+}
